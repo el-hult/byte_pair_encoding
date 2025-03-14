cd43e65ea2772743cc65c6070578a4f7d5ecc53d
Ludvig Hult <ludvig.hult@gmail.com>
2025-03-14 23:34:56 +0100

    backtrack -- remove the struct tat is not needed

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,37 @@
 use std::fs::read_to_string;
 
-struct TokenizedString { 
-    data: Vec<usize>
-}
+type TokenizedString = Vec<usize>;
 
-impl TokenizedString {
-    fn printme(&self) {
-        // TODO implement changing color between every second token, so that token boundaries becode clear
-        println!("lol");
-    }
-}
-
-fn decode(v:TokenizedString,mapping_table:Vec<char>) -> String {
+fn decode(v: TokenizedString, mapping_table: Vec<char>) -> String {
     let mut s = vec![];
-    for token in v.data{
+    for token in v {
         s.push(mapping_table[token]);
     }
     s.into_iter().collect::<String>()
 }
 
-fn encode(s: &str) -> (TokenizedString,Vec<char>){
-    let mut mapping_table = vec![];
+fn encode(s: &str) -> (TokenizedString, Vec<char>) {
+    let mut mapping_table: Vec<char> = vec![];
     let mut v = vec![];
-    for c in s.chars(){
-        let idx =  mapping_table.iter().position(|&a|a==c);
+    for c in s.chars() {
+        let idx = mapping_table.iter().position(|&a| a == c);
         match idx {
-            Some(i)=> v.push(i),
-            None=> {
+            Some(i) => v.push(i),
+            None => {
                 let i = mapping_table.len();
                 v.push(i);
                 mapping_table.push(c);
             }
         }
     }
-    (TokenizedString{data:v},mapping_table)
+    (v, mapping_table)
 }
 
 fn main() -> () {
     let s = read_to_string("input.txt").expect("file is there");
-    let (v,mapping_table) = encode(&s);
-    v.printme();
+    let (v, mapping_table) = encode(&s);
     let s2 = decode(v, mapping_table);
-    println!("{}",s);
-    println!("{}",s2);
+    println!("{}", s);
+    println!("{}", s2);
     ()
 }
