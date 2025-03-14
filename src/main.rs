a1840f192f5ff51eefe53cc1860272096b97bf01
Ludvig Hult <ludvig.hult@gmail.com>
2025-03-14 21:59:17 +0100

    provide most common pair explicitly

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,11 @@ fn collect_counts(s:&str)->HashMap<CharPair,u64> {
     hm
 }
 fn main() -> () {
-    let s = read_to_string("input.txt").expect("file is there");
+    let s = read_to_string("input.txt").expect("file is there").to_lowercase();
     let hm = collect_counts(&s);
-    print!("{:?}",hm);
+    let (most_common_pair,_) = hm.iter().max_by_key(
+        |(_,&c)| c
+    ).expect("There should be at least one pair");
+    print!("{:?}",most_common_pair);
     ()
 }
