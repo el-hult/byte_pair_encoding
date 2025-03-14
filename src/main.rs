df2ee58a3b195d047f3651e345b3728e2a1268a5
Ludvig Hult <ludvig.hult@gmail.com>
2025-03-14 21:53:40 +0100

    collect pair counts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string, iter::zip};
+
+type CharPair = (char,char);
+
+fn collect_counts(s:&str)->HashMap<CharPair,u64> {
+    let mut hm = HashMap::new();
+    let left = s.chars();
+    let right = s.chars().skip(1);
+    let pairs = zip(left,right);
+    for pair in pairs {
+        *hm.entry(pair).or_insert(0) += 1;
+    }
+    hm
+}
 fn main() -> () {
     let s = read_to_string("input.txt").expect("file is there");
-    print!("{}",s);
+    let hm = collect_counts(&s);
+    print!("{:?}",hm);
     ()
 }
