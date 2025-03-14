bc111276f26c94063bf27865e84e80b562dfdb2b
Ludvig Hult <ludvig.hult@gmail.com>
2025-03-14 23:48:27 +0100

    color output

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::fs::read_to_string;
-
+const ANSI_RESET: &str = "\x1b[0m";
+const ANSI_YELLOW_TEXT: &str = "\x1b[93m";
+//const ANSI_YELLOW_BACKGROUND: &str = "\x1b[43m";
 type TokenizedString = Vec<usize>;
 
-fn decode(v: TokenizedString, mapping_table: Vec<char>) -> String {
+fn decode<const COLOR: bool>(v: TokenizedString, mapping_table: Vec<char>) -> String {
     let mut s = vec![];
+    let mut is_red = false;
     for token in v {
+        if COLOR && is_red {
+            ANSI_YELLOW_TEXT.chars().for_each(|c| s.push(c));
+        }
         s.push(mapping_table[token]);
+        if COLOR && is_red {
+            ANSI_RESET.chars().for_each(|c| s.push(c));
+        }
+        if COLOR {
+            is_red = !is_red;
+        }
     }
     s.into_iter().collect::<String>()
 }
@@ -30,8 +42,7 @@ fn encode(s: &str) -> (TokenizedString, Vec<char>) {
 fn main() -> () {
     let s = read_to_string("input.txt").expect("file is there");
     let (v, mapping_table) = encode(&s);
-    let s2 = decode(v, mapping_table);
-    println!("{}", s);
+    let s2 = decode::<true>(v, mapping_table);
     println!("{}", s2);
     ()
 }
