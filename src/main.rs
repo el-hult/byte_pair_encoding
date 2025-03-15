64c0597479ae2d5035ed7250436353fef0b0b88f
Ludvig Hult <ludvig.hult@gmail.com>
2025-03-15 16:04:58 +0100

    move the dictionary logic in a struct to encapsulate -- preparing for move to byte level work

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::{collections::HashMap, fs::read_to_string};
 const ANSI_RESET: &str = "\x1b[0m";
 const ANSI_YELLOW_TEXT: &str = "\x1b[93m";
 
-
-type TokenizedString = Vec<usize>;
+type Token = usize;
+type TokenizedString = Vec<Token>;
 
 // TODO tokenize on the byte level, not on the unicode character level, to enable BPE encoding unseen unicode characters (but the basic dictionary is that all bytes are kept as they are anyways)
 // the TokenMap should have all reductions from (Token,Token) -> Token
@@ -11,10 +11,33 @@ type TokenizedString = Vec<usize>;
 // in the first step, use raw bytes casted to u16 as tokens, and then do BPE on that level
 // all the non-byte-tokens are from \x0100 and upwards wo we are guaranteed no ambiguity
 // a token is a u16 = (u8,u8)
-// and so the mapping table is HashMap<(u16,u16),u16>
+// and so the mapping table is HashMap<(u16,u16),u16> or maybe even HashMap<u32,u16> since we can just shift the u16s into a u32
 // and the reverse mapping table is HashMap<u16,Vec<u8>>
 // a hashmap is probably wasteful, but it is simple to implement and may be fast enough? since the key is really a u32 and all elements in range of u8 are invalid as keys, we could use a vector and just subtract \xFF from the key to get an index? should be super mega fast. but less readable and more code to write?
-type TokenMap = Vec<String>;
+struct Dictionary {
+    forwards: Vec<String>
+}
+
+impl Dictionary {
+    fn decode(&self, token: Token) -> String {
+        self.forwards[token].clone()
+    }
+    // TODO when working with bytes, we can use some bit manipulation to make this nicer
+    fn add_char_token(&mut self, char: char) -> Token {
+        self.forwards.push(char.to_string());
+        self.forwards.len() - 1
+    }
+    fn add_pair_rule(&mut self, (fst,snd): (&Token,&Token)) -> Token {
+        let new_token = self.decode(*fst) + &self.decode(*snd);
+        self.forwards.push(new_token);
+        self.forwards.len() - 1
+    }
+    // TODO this will be removed when I move to byte level encoding instead of characted level encoding
+    // TODO use a hash map instead to speed up the lookup?
+    fn get_token_for_char(&self, c: &char) -> Option<Token> {
+        self.forwards.iter().position(|x| x == &c.to_string())
+    }
+}
 
 
 
@@ -24,14 +47,14 @@ type TokenMap = Vec<String>;
 // -> decoding a token produces a valid unicode string -- set a color that alternates (white/yellow?)
 // -> decoding a token produces an invalid unicode string -- decode another token and see if the combined byte sequence is valid,
 //   if it is, set the color to red, otherwise add another token in decoding, and try again
-fn decode<const COLOR: bool>(v: &TokenizedString, mapping_table: &TokenMap) -> String {
+fn decode<const COLOR: bool>(v: &TokenizedString, mapping_table: &Dictionary) -> String {
     let mut s = vec![];
     let mut is_red = false;
     for token in v {
         if COLOR && is_red {
             s.push(ANSI_YELLOW_TEXT.to_string());
         }
-        s.push(mapping_table[*token].clone());
+        s.push(mapping_table.decode(*token));
         if COLOR && is_red {
             s.push(ANSI_RESET.to_string());
         }
@@ -42,53 +65,48 @@ fn decode<const COLOR: bool>(v: &TokenizedString, mapping_table: &TokenMap) -> S
     s.into_iter().collect::<String>()
 }
 
-fn encode(s: &str) -> (TokenizedString, TokenMap) {
-    let mut mapping_table: Vec<String> = vec![];
+fn encode(s: &str) -> (TokenizedString, Dictionary) {
+    let mut dict = Dictionary { forwards: vec![] };
     let mut v = vec![];
     for c in s.chars() {
-        // TODO use a map. this became slow after the number of tokens grew large enough for the vector to not fit in cache properly
-        let idx = mapping_table
-            .iter()
-            .position(|a| a.chars().next().unwrap() == c);
+        let idx = dict.get_token_for_char(&c);
         match idx {
             Some(i) => v.push(i),
             None => {
-                println!("{} => {}", c, mapping_table.len());
-                let i = mapping_table.len();
+                let i = dict.add_char_token(c);
                 v.push(i);
-                mapping_table.push(c.to_string());
             }
         }
     }
-    (v, mapping_table)
+    (v, dict)
 }
 
 /// Find the most common token pair, replace it in the tokenized string
-fn prune_round(v: &TokenizedString, tkn_map: &mut TokenMap) -> TokenizedString {
-    //println!("PRUNE");
+/// Return the new tokenized string, and the number of times the newly created token was used
+fn prune_round(v: &TokenizedString, tkn_map: &mut Dictionary) -> (TokenizedString, usize) {
     if v.len() <= 1 {
-        return v.clone();
+        return (v.clone(),0);
     }
 
-    let mut hm = HashMap::new();
-    let mut it = v.iter();
-    let mut fst = it.next().unwrap();
-    while let Some(snd) = it.next() {
-        let pair = (fst, snd);
-        *hm.entry(pair).or_insert(0) += 1;
-        fst = snd;
+    // count all token pairs
+    let mut hm: HashMap<(&Token, &Token), usize> = HashMap::new();
+    let mut token_iterator = v.iter();
+    let mut fst_token = token_iterator.next().unwrap();
+    while let Some(snd_token) = token_iterator.next() {
+        let token_pair = (fst_token, snd_token);
+        *hm.entry(token_pair).or_insert(0) += 1;
+        fst_token = snd_token;
     }
 
     // decide what the new token to remove is
-    let (max, count) = hm
+    // TODO make sure this is deterministic, and I don't fall into some trap about iteration order when iterating a hashmap. split ties on the keys!
+    let (max_pair, count) = hm
         .into_iter()
         .max_by_key(|(_, b)| *b)
         .expect("There should be at least one pair in the iteration before");
-    let new_token_number = tkn_map.len();
-    //println!("{}", new_token_number);
-    let new_token = tkn_map[*max.0].clone() + &tkn_map[*max.1];
-    println!("{} => {}", new_token, count);
-    tkn_map.push(new_token);
+    let new_token_number = tkn_map.add_pair_rule(max_pair);
+    let foo = tkn_map.decode(new_token_number);
+    println!("{} ({})", foo, count);
 
     // replace all occurances of the 'max' combination with a new token!
     let mut out = vec![];
@@ -97,7 +115,7 @@ fn prune_round(v: &TokenizedString, tkn_map: &mut TokenMap) -> TokenizedString {
     let mut rest_token = true;
     while let Some(snd) = it.next() {
         let pair = (fst, snd);
-        if pair == max {
+        if pair == max_pair {
             out.push(new_token_number);
             let maybe_fst = it.next();
             match maybe_fst {
@@ -116,19 +134,20 @@ fn prune_round(v: &TokenizedString, tkn_map: &mut TokenMap) -> TokenizedString {
         out.push(*fst);
     }
 
-    out
+    (out,count)
 }
 
 fn main() -> () {
     let s = read_to_string("input.txt").expect("file is there");
     let (mut v, mut table) = encode(&s);
     
-    // TODO tokenize until the least common token is used less than 10 times. this means we need to return an optional or a Result or something...
-    for _ in 0..200 {
-        v = prune_round(&v, &mut table);
+    // create new tokens until the usage count for new tokens is below 100
+    let mut times_used = 99999;
+    while times_used > 100 {
+        (v,times_used) = prune_round(&v, &mut table);
     }
     let s2 = decode::<true>(&v, &table);
     println!("{}", s2);
-    println!("{table:?}");
+    println!("{:?}", table.forwards);
     ()
 }
